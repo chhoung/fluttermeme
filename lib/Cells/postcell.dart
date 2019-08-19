2ce2f3619dc8443bdacc221eabd7c4077db0354b
Pheng Kimchhorng <phengchhoung@gmail.com>
2019-08-19 09:09:29 +0700

    delete comment

--- lib/Cells/postcell.dart
+++ lib/Cells/postcell.dart
@@ -7,7 +7,6 @@ class PostCell extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Column(
       children: <Widget>[
         new Container(
